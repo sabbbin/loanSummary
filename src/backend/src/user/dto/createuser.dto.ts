a08ed6e6860502362c7edec670a5775bacc0e4d2
prabhat-rimal <rimalprabhat49@gmail.com>
2022-08-22 17:42:06 +0545

    creating rest api and dto of user

---
+++ src/backend/src/user/dto/createuser.dto.ts
@@ -0,0 +1,13 @@
+import { Role } from "@prisma/client";
+import { IsEnum, IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  username: string;
+  @IsNotEmpty()
+  status: string;
+  @IsNotEmpty()
+  password: string;
+  @IsEnum(Role)
+  role: Role;
+}
