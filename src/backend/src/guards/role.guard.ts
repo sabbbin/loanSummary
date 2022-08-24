49c55d01d7ba413d1ea4eb8ed08a3155021fedbc
sabbbin <suwal721063@gmail.com>
2022-08-24 16:22:12 +0545

    feat:  adding user role

---
+++ src/backend/src/guards/role.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.get<string[]>(
+      'roles',
+      context.getHandler(),
+    );
+    if (!requiredRoles) {
+      return true;
+    }
+    const { user } = context.switchToHttp().getRequest();
+    console.log(user);
+
+    return requiredRoles.some((rol) => rol == user.role);
+  }
+}
